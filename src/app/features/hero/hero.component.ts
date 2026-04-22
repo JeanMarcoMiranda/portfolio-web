c362620a8ea233eb3e3b082bb0769318f1f43360
JeanMarcoMiranda <elecord2014@gmail.com>
2026-04-22 12:42:44 -0500

    feat: fixing import global styles and adding hero component

--- src/app/features/hero/hero.component.ts
+++ src/app/features/hero/hero.component.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  stats = [
+    { label: 'AÑOS EXP', value: '3+' },
+    { label: 'PROYECTOS', value: '10+' },
+    { label: 'TECNOLOGÍAS', value: '8+' },
+    { label: 'REMOTO', value: '100%' },
+  ];
+}
